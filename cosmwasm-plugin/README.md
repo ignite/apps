0cd16071e28cc3f9f5d3250c850750ad938936cd
yerasyl <yerasyl.amanbek@enodatech.com>
2023-08-31 18:24:34 +0100

    added cosmwasm plugin for Ignite CLI

---
+++ cosmwasm-plugin/README.md
@@ -0,0 +1,133 @@
+# CosmWasm Plugin for Ignite CLI
+
+**This repo contains** an Ignite CLI plugin that can help you to add CosmWasm module support for newly scaffolded app. 
+
+## Get started
+First scaffold a chain with [Ignite CLI](https://docs.ignite.com).
+
+1. Install Ignite CLI:
+```
+$ curl https://get.ignite.com/cli! | bash
+```
+2. Scaffold a chain
+```
+$ ignite scaffold chain planet
+$ cd planet
+```
+
+3. Add cosmwasm plugin within your project directory:
+```
+ignite plugin add <this repo>
+```
+
+4. Run command
+
+```
+$ ignite cosmwasm-plugin add
+```
+
+5. Launch your chain with CosmWasm support
+
+```
+$ ignite chain serve
+```
+`serve` command installs dependencies, builds, initializes, and starts your blockchain in development.
+
+
+## Testing Smart-Contracts
+
+Testing CosmWasm smart-contract capabilities in your chain, can be performed by following these steps:
+
+0. Prerequisites: install [rust](https://www.rust-lang.org/tools/install) and [docker](https://www.docker.com/) (for smart-contact optimization)
+
+1. Launch chain locally:
+```
+$ ignite chain serve
+```
+
+2. Prepare cosmwasm smart-contract for deployment
+```
+$ git clone https://github.com/CosmWasm/cw-examples
+$ cd cw-examples
+$ cd contracts/nameservice
+$ RUSTFLAGS='-C link-arg=-s' cargo wasm
+```
+
+Optimize contract to reduce gas
+```
+$ docker run --rm -v "$(pwd)":/code \
+  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  cosmwasm/rust-optimizer:0.12.12
+```
+
+For ARM64 machines:
+```
+$ docker run --rm -v "$(pwd)":/code \
+  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  cosmwasm/rust-optimizer-arm64:0.12.12
+```
+
+3. Deploy smart-contract to your chain
+```
+$ planetd tx wasm store artifacts/cw_nameservice.wasm --from alice --gas=10000000 
+```
+
+4. Check your smart-contract on chain
+```
+$ planetd query wasm list-code
+code_infos:
+- code_id: "1"
+  creator: cosmos1a4w3vwxa8zhy57nyy9kw7zhdasr4ue4wld0zpn
+  data_hash: DF3C9BC1341322810523AABCA28CC3FCDCA021C85061743967CE3D20F5580093
+pagination: {}
+
+# download wasm and diff with origin
+$ CODE_ID=$(planetd query wasm list-code --output json | jq -r '.code_infos[0].code_id')
+$ planetd query wasm code $CODE_ID download.wasm
+$ diff artifacts/cw_nameservice.wasm download.wasm
+```
+
+5. Instantiate smart-contract, in CosmWasm deployment and instantiation are 2 different steps
+
+```
+$ INIT='{"purchase_price":{"amount":"100","denom":"stake"},"transfer_price":{"amount":"999","denom":"stake"}}'
+$ planetd tx wasm instantiate 1 "$INIT" --from alice --chain-id "planet" --label "awesome name service" --no-admin
+
+$ CONTRACT=$(planetd query wasm list-contract-by-code $CODE_ID --output json | jq -r '.contracts[-1]')
+# check contract state (you should get contract address)
+$ planetd query wasm contract $CONTRACT
+address: cosmos14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s0phg4d
+contract_info:
+  code_id: "1"
+  creator: cosmos1a4w3vwxa8zhy57nyy9kw7zhdasr4ue4wld0zpn
+  label: awesome name service
+```
+
+6. Interact with smart-contract
+```
+# purchase name
+$ planetd tx wasm execute $CONTRACT '{"register":{"name":"test"}}' --amount 100stake --from alice $TXFLAG -y
+
+# query registered name (you should see your address as owner of the name)
+$ NAME_QUERY='{"resolve_record": {"name": "test"}}'
+$ planetd query wasm contract-state smart $CONTRACT "$NAME_QUERY" --output json
+```
+
+## Compatibility
+
+| Ignite CLI  | Cosmos SDK  | IBC       | wasmd                                                         |
+|-------------|-------------|-----------|---------------------------------------------------------------|
+| v0.27.1     | v0.47.4     | v7.2.0    | v0.41.0                                                       |
+
+## Learn more
+
+- [Ignite CLI](https://ignite.com/cli)
+- [Tutorials](https://docs.ignite.com/guide)
+- [Ignite CLI docs](https://docs.ignite.com)
+- [Cosmos SDK docs](https://docs.cosmos.network)
+- [Developer Chat](https://discord.gg/ignite)
+- [CosmWasm](https://cosmwasm.com/)
+- [Wasm module](https://github.com/CosmWasm/wasmd)
+- [CosmWasm Smart Contract Tutorial](https://medium.com/haderech-dev/smart-contract-tutorial-3-cosmwasm-805860c91a88)
--- cosmwasm-plugin/main.go
+++ cosmwasm-plugin/main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"encoding/gob"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 
 	hplugin "github.com/hashicorp/go-plugin"
 
 	"github.com/ignite/cli/ignite/services/chain"
 	"github.com/ignite/cli/ignite/services/plugin"
 )
+
+var chainName string
+var chainNamed string
 
 func init() {
 	gob.Register(plugin.Manifest{})
@@ -34,13 +41,14 @@ func (p) Manifest() (plugin.Manifest, error) {
 				},
 				PlaceCommandUnder: "ignite",
 				// Examples of adding subcommands:
-				/*
-					Commands: []plugin.Command{
-						{Use: "add"},
-						{Use: "list"},
-						{Use: "delete"},
+				Commands: []plugin.Command{
+					{
+						Use:               "add",
+						Short:             "Add a CosmWasm module to the chain",
+						Long:              "This command will install dependencies, create new files, and modify existing files to add a CosmWasm module to the chain.",
+						PlaceCommandUnder: "ignite cosmwasm-plugin",
 					},
-				*/
+				},
 			},
 		},
 		// Add hooks here
@@ -50,28 +58,40 @@ func (p) Manifest() (plugin.Manifest, error) {
 }
 
 func (p) Execute(cmd plugin.ExecutedCommand) error {
-	// TODO: write command execution here
-	fmt.Printf("Hello I'm the cosmwasm-plugin plugin\n")
-	fmt.Printf("My executed command: %q\n", cmd.Path)
-	fmt.Printf("My args: %v\n", cmd.Args)
-	myFlag, _ := cmd.Flags().GetString("my-flag")
-	fmt.Printf("My flags: my-flag=%q\n", myFlag)
-	fmt.Printf("My config parameters: %v\n", cmd.With)
+
+	fmt.Printf("Hello I'm the cosmwasm-plugin\n")
 
 	// This is how the plugin can access the chain:
 	// c, err := getChain(cmd)
 
 	// According to the number of declared commands, you may need a switch:
-	/*
-		switch cmd.Use {
-		case "add":
-			fmt.Println("Adding stuff...")
-		case "list":
-			fmt.Println("Listing stuff...")
-		case "delete":
-			fmt.Println("Deleting stuff...")
-		}
-	*/
+
+	switch cmd.Use {
+	case "add":
+		return handleAddCommand()
+	default:
+		return fmt.Errorf("unknown command: %s", cmd.Use)
+	}
+
+	return nil
+}
+
+func handleAddCommand() error {
+	fmt.Println("Adding stuff...")
+
+	err := installDependencies()
+	if err != nil {
+		return err
+	}
+
+	err = createNewFiles()
+	if err != nil {
+		return err
+	}
+
+	//err = modifyExistingFiles()
+	//if err != nil {return err}
+
 	return nil
 }
 
@@ -90,6 +110,7 @@ func (p) ExecuteHookCleanUp(hook plugin.ExecutedHook) error {
 	return nil
 }
 
+// For future use
 func getChain(cmd plugin.ExecutedCommand, chainOption ...chain.Option) (*chain.Chain, error) {
 	var (
 		home, _ = cmd.Flags().GetString("home")
@@ -105,7 +126,140 @@ func getChain(cmd plugin.ExecutedCommand, chainOption ...chain.Option) (*chain.C
 	return chain.New(absPath, chainOption...)
 }
 
+func installDependencies() error {
+	cmd := exec.Command("go", "get", "github.com/CosmWasm/wasmd/x/wasm@v0.41.0")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to install dependency: %w", err)
+	}
+	return nil
+}
+
+func replaceWordsInFile(filePath, targetWord, replacement string) error {
+	// Read content from the file
+	content, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	// Perform the replacement
+	modifiedContent := strings.ReplaceAll(string(content), targetWord, replacement)
+
+	// Write the modified content back to the file
+	err = ioutil.WriteFile(filePath, []byte(modifiedContent), 0644)
+	if err != nil {
+		return err
+	}
+
+	//fmt.Printf("Replaced planet with %s\n", replacement)
+
+	return nil
+}
+
+func createFile(inputFilename, outputDir, outputFilename string) error {
+	sourcePath := filepath.Join("..", "cosmwasm-plugin", "wasm-wiring", inputFilename)
+
+	sourceContent, err := ioutil.ReadFile(sourcePath)
+	if err != nil {
+		return err
+	}
+
+	outputPath := filepath.Join(outputDir, outputFilename)
+
+	err = ioutil.WriteFile(outputPath, sourceContent, 0644)
+	if err != nil {
+		return err
+	}
+
+	//replace chainName
+	err = replaceWordsInFile(outputPath, "planet", chainName)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Created %s in %s\n", outputFilename, outputDir)
+	return nil
+}
+
+func createNewFiles() error {
+	// Create ante.go based on ante.txt content
+	err := createFile("ante.txt", "app", "ante.go")
+	if err != nil {
+		return err
+	}
+
+	// Create ante2.go based on wasm.txt content
+	err = createFile("wasm.txt", "app", "wasm.go")
+	if err != nil {
+		return err
+	}
+
+	// Create network.go based on network.txt content
+	err = createFile("app.txt", "app", "app.go")
+	if err != nil {
+		return err
+	}
+
+	// Create network.go based on network.txt content
+	err = createFile("simulation_test.txt", "app", "simulation_test.go")
+	if err != nil {
+		return err
+	}
+
+	// Create network.go based on network.txt content
+	err = createFile("network.txt", "testutil/network", "network.go")
+	if err != nil {
+		return err
+	}
+
+	filePath := filepath.Join("cmd", chainNamed, "cmd")
+	// Create network.go based on network.txt content
+	err = createFile("root.txt", filePath, "root.go")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// For future versions..
+func modifyExistingFiles() error {
+	// Modify existingFile1.go
+	content, err := os.ReadFile("existingFile1.go")
+	if err != nil {
+		return err
+	}
+	modifiedContent := string(content) + "\n// Additional content for existingFile1.go"
+	err = os.WriteFile("existingFile1.go", []byte(modifiedContent), 0644)
+	if err != nil {
+		return err
+	}
+
+	// Similarly, modify other existing files...
+
+	return nil
+}
+
 func main() {
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Extract the chain name from the current directory
+	chainName = filepath.Base(currentDir)
+
+	// Handle potential cases where the chain name contains dashes or underscores
+	chainName = strings.ReplaceAll(chainName, "-", "")
+	chainName = strings.ReplaceAll(chainName, "_", "")
+	chainNamed = chainName + "d"
+
+	//fmt.Println("User's chain name:", chainName)
+
 	pluginMap := map[string]hplugin.Plugin{
 		"cosmwasm-plugin": &plugin.InterfacePlugin{Impl: &p{}},
 	}
