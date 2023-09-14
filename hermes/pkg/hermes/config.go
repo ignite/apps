ddf6ffc8a6d9d045575d0430d94884713c3b5c87
jeronimoalbi <jeronimo.albi@gmail.com>
2023-09-14 12:26:03 +0200

    chore: remove directive comment to fix CI error

--- hermes/pkg/hermes/config.go
+++ hermes/pkg/hermes/config.go
@@ -392,7 +392,7 @@ func WithChainMaxGas(maxGas uint64) ChainOption {
 // WithChainGasPrice set the chain gas price into the Hermes config.
 func WithChainGasPrice(price sdk.Coin) ChainOption {
 	return func(c *Chain) {
-		f, _ := price.Amount.BigInt().Float64() //nolint:typecheck
+		f, _ := price.Amount.BigInt().Float64()
 		c.GasPrice = GasPrice{
 			Denom: price.Denom,
 			Price: f,
