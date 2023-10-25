1d8090ce09e36c79333b2e7c1521e712a8a527e1
jeronimoalbi <jeronimo.albi@gmail.com>
2023-10-25 15:46:03 +0200

    refactor: update apps to use gRPC

--- explorer/main.go
+++ explorer/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/gob"
+	"context"
 	"os"
 
 	hplugin "github.com/hashicorp/go-plugin"
@@ -10,51 +10,42 @@ import (
 	"github.com/ignite/apps/explorer/cmd"
 )
 
-func init() {
-	gob.Register(plugin.Manifest{})
-	gob.Register(plugin.ExecutedCommand{})
-	gob.Register(plugin.ExecutedHook{})
-}
+type app struct{}
 
-type p struct{}
-
-func (p) Manifest() (plugin.Manifest, error) {
-	m := plugin.Manifest{
-		Name: "explorer",
-	}
+func (app) Manifest(context.Context) (*plugin.Manifest, error) {
+	m := &plugin.Manifest{Name: "explorer"}
 	m.ImportCobraCommand(cmd.NewExplorer(), "ignite")
 	return m, nil
 }
 
-func (p) Execute(c plugin.ExecutedCommand) error {
+func (app) Execute(_ context.Context, c *plugin.ExecutedCommand, _ plugin.ClientAPI) error {
 	// Instead of a switch on c.Use, we run the root command like if
 	// we were in a command line context. This implies to set os.Args
 	// correctly.
 	// Remove the first arg "ignite" from OSArgs because our explorer
 	// command root is "explorer" not "ignite".
-	os.Args = c.OSArgs[1:]
+	os.Args = c.OsArgs[1:]
 	return cmd.NewExplorer().Execute()
 }
 
-func (p) ExecuteHookPre(plugin.ExecutedHook) error {
+func (app) ExecuteHookPre(context.Context, *plugin.ExecutedHook, plugin.ClientAPI) error {
 	return nil
 }
 
-func (p) ExecuteHookPost(plugin.ExecutedHook) error {
+func (app) ExecuteHookPost(context.Context, *plugin.ExecutedHook, plugin.ClientAPI) error {
 	return nil
 }
 
-func (p) ExecuteHookCleanUp(plugin.ExecutedHook) error {
+func (app) ExecuteHookCleanUp(context.Context, *plugin.ExecutedHook, plugin.ClientAPI) error {
 	return nil
 }
 
 func main() {
-	pluginMap := map[string]hplugin.Plugin{
-		"explorer": &plugin.InterfacePlugin{Impl: &p{}},
-	}
-
 	hplugin.Serve(&hplugin.ServeConfig{
 		HandshakeConfig: plugin.HandshakeConfig(),
-		Plugins:         pluginMap,
+		Plugins: map[string]hplugin.Plugin{
+			"explorer": plugin.NewGRPC(&app{}),
+		},
+		GRPCServer: hplugin.DefaultGRPCServer,
 	})
 }
--- hermes/go.mod
+++ hermes/go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/golangci/golangci-lint v1.54.2
 	github.com/gookit/color v1.5.4
 	github.com/hashicorp/go-plugin v1.5.1
-	github.com/ignite/cli v0.27.1
+	github.com/ignite/cli v0.27.2
 	github.com/ignite/ignite-files/hermes v0.0.1
 	github.com/manifoldco/promptui v0.9.0
 	github.com/pelletier/go-toml/v2 v2.1.0
@@ -303,14 +303,14 @@ require (
 	go.uber.org/goleak v1.2.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.24.0 // indirect
-	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/exp v0.0.0-20230711153332-06a737ee72cb // indirect
 	golang.org/x/exp/typeparams v0.0.0-20230307190834-24139beb5833 // indirect
 	golang.org/x/mod v0.12.0 // indirect
-	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/term v0.12.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/genproto v0.0.0-20230706204954-ccb25ca9f130 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20230629202037-9506855d4529 // indirect
