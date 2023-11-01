6c23e4627fcb92510146aebc9000daef637d3dc4
yerasyl <yerasyl.amanbek@enodatech.com>
2023-11-01 11:04:15 +0000

    applied code injectin to app.go (with app6-10 plush files) using ASTs

--- cosmwasm/helper.go
+++ cosmwasm/helper.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"go/ast"
 	"go/format"
@@ -204,6 +205,158 @@ func modifyFilesHelper(outputDir, outputFilename string, chainName string) error
 		return true
 	})
 
+	//appending app6.plush and app7.plush
+	ast.Inspect(node, func(n ast.Node) bool {
+
+		fs := token.NewFileSet()
+
+		typeSpec, ok := n.(*ast.TypeSpec)
+		if !ok || typeSpec.Name.Name != "App" {
+			return true
+		}
+
+		structType, ok := typeSpec.Type.(*ast.StructType)
+		if !ok {
+			return true
+		}
+
+		// Identify the correct position to insert fields
+		position := -1
+		for idx, field := range structType.Fields.List {
+			if len(field.Names) > 0 && field.Names[0].Name == "ConsensusParamsKeeper" {
+				position = idx
+				break
+			}
+		}
+
+		if position == -1 {
+			fmt.Println("Failed to find the correct insertion position.")
+			return false
+		}
+
+		// Parse the placeholder content into AST
+		placeholderAST, err := parser.ParseFile(fs, "", "package main; type _dummyStruct struct {"+string(placeholderContents[5])+"}", 0)
+		if err != nil {
+			panic(err)
+		}
+
+		// Extract the fields from the parsed placeholder
+		placeholderFields := placeholderAST.Decls[0].(*ast.GenDecl).Specs[0].(*ast.TypeSpec).Type.(*ast.StructType).Fields.List
+
+		// Insert fields at the correct position
+		structType.Fields.List = append(structType.Fields.List[:position+1], append(placeholderFields, structType.Fields.List[position+1:]...)...)
+
+		// app7.plush
+		// Parse the placeholder content into AST
+		placeholderAST, err = parser.ParseFile(fs, "", "package main; type _dummyStruct struct {"+string(placeholderContents[6])+"}", 0)
+		if err != nil {
+			panic(err)
+		}
+
+		// Extract the fields from the parsed placeholder
+		placeholderFields = placeholderAST.Decls[0].(*ast.GenDecl).Specs[0].(*ast.TypeSpec).Type.(*ast.StructType).Fields.List
+
+		for idx, field := range structType.Fields.List {
+			if len(field.Names) > 0 && field.Names[0].Name == "ScopedICAHostKeeper" {
+				structType.Fields.List = append(structType.Fields.List[:idx+1], append(placeholderFields, structType.Fields.List[idx+1:]...)...)
+				return false
+			}
+		}
+
+		return false
+	})
+
+	// appending app8.plush andd app9.plush
+	ast.Inspect(node, func(n ast.Node) bool {
+		fs := token.NewFileSet()
+
+		// Parse the placeholder content into AST
+		placeholderAST, err := parser.ParseFile(fs, "", "package main; func _dummyFunc("+string(placeholderContents[7])+") {}", 0)
+		if err != nil {
+			panic(err)
+		}
+
+		// Check if the current node is a function declaration
+		funcDecl, ok := n.(*ast.FuncDecl)
+		if !ok || funcDecl.Name.Name != "New" {
+			return true
+		}
+
+		// Find the parameter 'loadLatest bool' and insert after it
+		for i, param := range funcDecl.Type.Params.List {
+			if len(param.Names) > 0 && param.Names[0].Name == "loadLatest" {
+				// Insert new parameter after it
+				funcDecl.Type.Params.List = append(funcDecl.Type.Params.List, nil)
+				copy(funcDecl.Type.Params.List[i+2:], funcDecl.Type.Params.List[i+1:])
+				funcDecl.Type.Params.List[i+1] = placeholderAST.Decls[0].(*ast.FuncDecl).Type.Params.List[0]
+				break
+			}
+
+		}
+
+		placeholderAST, err = parser.ParseFile(fs, "", "package main; func _dummyFunc("+string(placeholderContents[8])+") {}", 0)
+		if err != nil {
+			panic(err)
+		}
+		// Find the parameter 'appOpts servertypes.AppOptions,' and insert after it
+		for i, param := range funcDecl.Type.Params.List {
+			if len(param.Names) > 0 && param.Names[0].Name == "appOpts" {
+				// Insert new parameter after it
+				funcDecl.Type.Params.List = append(funcDecl.Type.Params.List, nil)
+				copy(funcDecl.Type.Params.List[i+2:], funcDecl.Type.Params.List[i+1:])
+				funcDecl.Type.Params.List[i+1] = placeholderAST.Decls[0].(*ast.FuncDecl).Type.Params.List[0]
+				break
+			}
+		}
+
+		return true
+	})
+	// appending app10.plush
+	ast.Inspect(node, func(n ast.Node) bool {
+		assignStmt, ok := n.(*ast.AssignStmt)
+		if !ok {
+			return true
+		}
+
+		if len(assignStmt.Rhs) == 0 {
+			return true
+		}
+
+		callExpr, ok := assignStmt.Rhs[0].(*ast.CallExpr)
+		if !ok {
+			return true
+		}
+
+		if selExpr, ok := callExpr.Fun.(*ast.SelectorExpr); ok {
+			if selExpr.Sel.Name == "NewKVStoreKeys" {
+				fmt.Println("Found NewKVStoreKeys")
+
+				for idx, arg := range callExpr.Args {
+
+					// Convert the argument to its string representation
+					var buf bytes.Buffer
+					err := format.Node(&buf, fset, arg)
+					if err != nil {
+						panic(err)
+					}
+
+					if buf.String() == chainName+"moduletypes.StoreKey" {
+
+						// Create a new AST node for our placeholder content.
+						newKey := &ast.Ident{
+							Name: string(placeholderContents[9]),
+						}
+						// Insert our new key right after the located key.
+						callExpr.Args = append(callExpr.Args[:idx+1], append([]ast.Expr{newKey}, callExpr.Args[idx+1:]...)...)
+						return false
+					}
+				}
+			}
+		}
+
+		return true
+	})
+
 	// Write the modified AST back to the file.
 	outputFile, err := os.Create(filepath.Join(outputDir, outputFilename))
 	if err != nil {
@@ -222,6 +375,7 @@ func modifyFilesHelper(outputDir, outputFilename string, chainName string) error
 
 }
 
+// helper for app4.plush injextions
 func getModulesFromPlaceholder(fset *token.FileSet, content string) ([]ast.Expr, error) {
 	moduleStrings := strings.Split(content, ",")
 	var modules []ast.Expr
@@ -236,4 +390,33 @@ func getModulesFromPlaceholder(fset *token.FileSet, content string) ([]ast.Expr,
 	}
 
 	return modules, nil
+}
+
+func exprToString(fset *token.FileSet, expr ast.Expr) string {
+	var buf bytes.Buffer
+	format.Node(&buf, fset, expr)
+	return buf.String()
+}
+
+// formatNode formats a given ast.Node to a string.
+func formatNode(fset *token.FileSet, node ast.Node) string {
+	var buf bytes.Buffer
+	if err := format.Node(&buf, fset, node); err != nil {
+		return "<error formatting node>"
+	}
+	return buf.String()
+}
+
+func resetPos(node ast.Node) {
+	ast.Inspect(node, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.Ident:
+			x.NamePos = token.NoPos
+		case *ast.StarExpr:
+			x.Star = token.NoPos
+		case *ast.Field:
+			x.Doc = nil // remove any documentation associated with the field
+		}
+		return true
+	})
 }
