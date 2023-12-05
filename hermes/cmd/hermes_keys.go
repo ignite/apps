bddb3fa0047245fb2656fa201269f18e95ca7132
Jerónimo Albi <jeronimo.albi@gmail.com>
2023-12-05 10:10:43 +0100

    feat: expose Hermes keys list and delete commands (#30)
    * feat: expose Hermes keys list and delete commands
    
    * chore: change output format to plaintext instead of JSON
    
    Using plaintext as output makes it easier for users to interact with
    the Hermes app. The app should not be used in production so removing
    JSON should be fine. Production environments must use Hermes directly.

--- hermes/cmd/hermes_keys.go
+++ hermes/cmd/hermes_keys.go
@@ -13,14 +13,16 @@ func NewHermesKeys() *cobra.Command {
 		Short: "Manage the Hermes keys",
 	}
 	c.AddCommand(
-		NewHermesKeyAddMnemonic(),
-		NewHermesKeyAddFile(),
+		NewHermesKeysAddMnemonic(),
+		NewHermesKeysAddFile(),
+		NewHermesKeysList(),
+		NewHermesKeysDelete(),
 	)
 	return c
 }
 
-// NewHermesKeyAddMnemonic add a hermes relayer mnemonic key.
-func NewHermesKeyAddMnemonic() *cobra.Command {
+// NewHermesKeysAddMnemonic add a hermes relayer mnemonic key.
+func NewHermesKeysAddMnemonic() *cobra.Command {
 	c := &cobra.Command{
 		Use:   "add [chain-id] [mnemonic]",
 		Short: "Add a new key from mnemonic to Hermes relayer",
@@ -43,8 +45,8 @@ func NewHermesKeyAddMnemonic() *cobra.Command {
 	return c
 }
 
-// NewHermesKeyAddFile add a hermes relayer key file.
-func NewHermesKeyAddFile() *cobra.Command {
+// NewHermesKeysAddFile add a hermes relayer key file.
+func NewHermesKeysAddFile() *cobra.Command {
 	c := &cobra.Command{
 		Use:   "file [chain-id] [filepath]",
 		Short: "Add a new key from a key file to Hermes relayer",
@@ -57,6 +59,53 @@ func NewHermesKeyAddFile() *cobra.Command {
 			defer h.Cleanup()
 
 			return h.AddKey(
+				cmd.Context(),
+				args[0],
+				args[1],
+				hermes.WithStdOut(cmd.OutOrStdout()),
+			)
+		},
+	}
+	return c
+}
+
+// NewHermesKeysList list hermes relayer keys.
+func NewHermesKeysList() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "list [chain-id]",
+		Short: "List Hermes relayer keys",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			h, err := hermes.New()
+			if err != nil {
+				return err
+			}
+			defer h.Cleanup()
+
+			return h.KeysList(
+				cmd.Context(),
+				args[0],
+				hermes.WithStdOut(cmd.OutOrStdout()),
+			)
+		},
+	}
+	return c
+}
+
+// NewHermesKeysDelete deletes a hermes relayer key.
+func NewHermesKeysDelete() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "delete [chain-id] [key-name]",
+		Short: "Delete a key from Hermes relayer",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			h, err := hermes.New()
+			if err != nil {
+				return err
+			}
+			defer h.Cleanup()
+
+			return h.DeleteKey(
 				cmd.Context(),
 				args[0],
 				args[1],
