f47e259f0486c52ed2c7f1532713ae338f558e40
ehsan <ehsan.saradar@gmail.com>
2024-01-30 16:00:04 +0330

    feat: Add example app demonstrating how to use flags in Ignite apps

---
+++ examples/flags/integration/app_test.go
@@ -0,0 +1,88 @@
+package integration_test
+
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	pluginsconfig "github.com/ignite/cli/v28/ignite/config/plugins"
+	"github.com/ignite/cli/v28/ignite/pkg/cmdrunner/step"
+	"github.com/ignite/cli/v28/ignite/services/plugin"
+	envtest "github.com/ignite/cli/v28/integration"
+)
+
+func TestFlags(t *testing.T) {
+	var (
+		require = require.New(t)
+		env     = envtest.New(t)
+		app     = env.Scaffold("github.com/test/test")
+	)
+
+	dir, err := os.Getwd()
+	require.NoError(err)
+	pluginPath := filepath.Join(filepath.Dir(filepath.Dir(dir)), "flags")
+
+	env.Must(env.Exec("install flags app locally",
+		step.NewSteps(step.New(
+			step.Exec(envtest.IgniteApp, "app", "install", pluginPath),
+			step.Workdir(app.SourcePath()),
+		)),
+	))
+
+	// One local plugin expected
+	assertLocalPlugins(t, app, []pluginsconfig.Plugin{
+		{
+			Path: pluginPath,
+		},
+	})
+	assertGlobalPlugins(t, app, nil)
+
+	buf := &bytes.Buffer{}
+	env.Must(env.Exec("run hello",
+		step.NewSteps(step.New(
+			step.Exec(
+				envtest.IgniteApp,
+				"flags",
+				"hello",
+				"--name",
+				"Test",
+			),
+			step.Workdir(app.SourcePath()),
+			step.Stdout(buf),
+		)),
+	))
+	require.Equal("Hello, Test!\n", buf.String())
+
+	buf.Reset()
+	env.Must(env.Exec("run cowsay",
+		step.NewSteps(step.New(
+			step.Exec(
+				envtest.IgniteApp,
+				"flags",
+				"cowsay",
+				"--name",
+				"Test",
+			),
+			step.Workdir(app.SourcePath()),
+			step.Stdout(buf),
+		)),
+	))
+	require.Contains(buf.String(), "Hello, Test!")
+}
+
+func assertLocalPlugins(t *testing.T, app envtest.App, expectedPlugins []pluginsconfig.Plugin) {
+	cfg, err := pluginsconfig.ParseDir(app.SourcePath())
+	require.NoError(t, err)
+	require.ElementsMatch(t, expectedPlugins, cfg.Apps, "unexpected local apps")
+}
+
+func assertGlobalPlugins(t *testing.T, app envtest.App, expectedPlugins []pluginsconfig.Plugin) {
+	cfgPath, err := plugin.PluginsPath()
+	require.NoError(t, err)
+	cfg, err := pluginsconfig.ParseDir(cfgPath)
+	require.NoError(t, err)
+	require.ElementsMatch(t, expectedPlugins, cfg.Apps, "unexpected global apps")
+}
---
+++ examples/flags/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	hplugin "github.com/hashicorp/go-plugin"
+
+	"flags/cmd"
+
+	"github.com/ignite/cli/v28/ignite/services/plugin"
+)
+
+type app struct{}
+
+func (app) Manifest(_ context.Context) (*plugin.Manifest, error) {
+	return &plugin.Manifest{
+		Name:     "flags",
+		Commands: cmd.GetCommands(),
+	}, nil
+}
+
+func (app) Execute(ctx context.Context, c *plugin.ExecutedCommand, _ plugin.ClientAPI) error {
+	// Remove the first two elements "ignite" and "flags" from OsArgs.
+	args := c.OsArgs[2:]
+
+	switch args[0] {
+	case "hello":
+		return cmd.ExecuteHello(ctx, c)
+	case "cowsay":
+		return cmd.ExecuteCowsay(ctx, c)
+	default:
+		return fmt.Errorf("unknown command: %s", c.Path)
+	}
+}
+
+func (app) ExecuteHookPre(_ context.Context, _ *plugin.ExecutedHook, _ plugin.ClientAPI) error {
+	return nil
+}
+
+func (app) ExecuteHookPost(_ context.Context, _ *plugin.ExecutedHook, _ plugin.ClientAPI) error {
+	return nil
+}
+
+func (app) ExecuteHookCleanUp(_ context.Context, _ *plugin.ExecutedHook, _ plugin.ClientAPI) error {
+	return nil
+}
+
+func main() {
+	hplugin.Serve(&hplugin.ServeConfig{
+		HandshakeConfig: plugin.HandshakeConfig(),
+		Plugins: map[string]hplugin.Plugin{
+			"flags": plugin.NewGRPC(&app{}),
+		},
+		GRPCServer: hplugin.DefaultGRPCServer,
+	})
+}
