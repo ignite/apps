be0006a4dce5199498e1ec8565b69d93a2e9aa0a
Ehsan-saradar <ehsan.saradar@gmail.com>
2024-02-01 22:58:08 +0330

    feat: Add marketplace app (#32)
    * add marketplace
    
    * Update go.work
    
    * Rename go.work to go.work.example
    
    * Use AppsConfig from ignite cli
    
    * Add test for tree view
    
    * Remove tools.go
    
    * Refactor test cases

---
+++ official/marketplace/cmd/marketplace.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+const (
+	githubTokenFlag = "github-token"
+)
+
+// NewMarketplace creates a new marketplace command that holds
+// some other sub commands related to running marketplace like
+// list and info.
+func NewMarketplace() *cobra.Command {
+	c := &cobra.Command{
+		Use:     "marketplace [command]",
+		Aliases: []string{"mp"},
+		Short:   "Run marketplace commands",
+		Long: `Marketplace is a command line tool that helps you to search for ignite apps
+			using GitHub search API. It also helps you to get more information about an app.
+			Please note that Github API has a very limited rate limit for unauthenticated requests
+			so it's recommended to use the --github-token flag you want to use marketplace commands frequently.`,
+		SilenceUsage:  true,
+		SilenceErrors: true,
+	}
+
+	// add sub commands.
+	c.AddCommand(
+		NewList(),
+		NewInfo(),
+	)
+
+	c.PersistentFlags().String(githubTokenFlag, "", "GitHub access token")
+
+	return c
+}
