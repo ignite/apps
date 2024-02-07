577ec76cc41c4ee3cb24ccda1feb255e2babca88
Ehsan-saradar <ehsan.saradar@gmail.com>
2024-02-07 12:28:24 +0330

    feat: Add Example App for Using chain-info in Ignite Apps (#48)
    * feat: Add example app demonstrating how to use chain-info in Ignite apps
    
    * add readme
    
    * use t.Helper()
    
    * udpate ignite
    
    * sort imports
    
    * Update examples/chain-info/readme.md
    
    Co-authored-by: Jerónimo Albi <jeronimo.albi@gmail.com>
    
    ---------
    
    Co-authored-by: Jerónimo Albi <jeronimo.albi@gmail.com>

---
+++ examples/chain-info/cmd/cmd.go
@@ -0,0 +1,32 @@
+package cmd
+
+import "github.com/ignite/cli/v28/ignite/services/plugin"
+
+// GetCommands returns the list of chain-info app commands.
+func GetCommands() []*plugin.Command {
+	return []*plugin.Command{
+		{
+			Use:   "chain-info",
+			Short: "chain-info is a simple application that demonstrates how to get information or manipulate the chains",
+			Commands: []*plugin.Command{
+				{
+					Use:   "info",
+					Short: "Prints out some basic informations about the chain in the current directory",
+				},
+				{
+					Use:   "build",
+					Short: "Builds the chain app in the current directory with help of ignite helper functions",
+					Flags: []*plugin.Flag{
+						{
+							Name:         "output",
+							Shorthand:    "o",
+							Usage:        "The path to output binary file",
+							DefaultValue: ".",
+							Type:         plugin.FlagTypeString,
+						},
+					},
+				},
+			},
+		},
+	}
+}
