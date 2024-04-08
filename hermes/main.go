e5d0ba856956f65ff4c21641a9ab25c93e66058f
Pantani <Pantani>
2024-04-09 01:17:42 +0200

    put hermes under root cmd

--- hermes/main.go
+++ hermes/main.go
@@ -14,15 +14,15 @@ type app struct{}
 
 func (app) Manifest(context.Context) (*plugin.Manifest, error) {
 	m := &plugin.Manifest{Name: "hermes"}
-	m.ImportCobraCommand(cmd.NewHermes(), "ignite relayer")
+	m.ImportCobraCommand(cmd.NewHermes(), "ignite")
 	return m, nil
 }
 
 func (app) Execute(_ context.Context, c *plugin.ExecutedCommand, _ plugin.ClientAPI) error {
 	// Run the "hermes" command as if it were a root command. To do
-	// so remove the first two arguments which are "ignite relayer"
+	// so remove the first two arguments which are "ignite"
 	// from OSArgs to treat "hermes" as the root command.
-	os.Args = c.OsArgs[2:]
+	os.Args = c.OsArgs[1:]
 	return cmd.NewHermes().Execute()
 }
 
