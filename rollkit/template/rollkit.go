ea985fb5e8d6ab724787f2c0ad4b979c7879fc22
Julien Robert <julien@rbrt.fr>
2024-05-09 12:12:19 +0200

    add integration tests

--- rollkit/template/rollkit.go
+++ rollkit/template/rollkit.go
@@ -1,12 +1,14 @@
 package template
 
 import (
+	"errors"
 	"path/filepath"
 	"strings"
 
 	"github.com/gobuffalo/genny/v2"
 	"github.com/gobuffalo/plush/v4"
 
+	"github.com/ignite/cli/v28/ignite/pkg/cosmosver"
 	"github.com/ignite/cli/v28/ignite/pkg/xast"
 	"github.com/ignite/cli/v28/ignite/pkg/xgenny"
 	"github.com/ignite/cli/v28/ignite/services/chain"
@@ -25,18 +27,26 @@ func NewRollKitGenerator(chain *chain.Chain) (*genny.Generator, error) {
 		return nil, err
 	}
 
-	g.RunFn(commandsModify(chain.AppPath(), binaryName))
+	g.RunFn(commandsModify(chain.AppPath(), binaryName, chain.Version))
 
 	return g, nil
 }
 
 // commandsModify modifies the application start to use rollkit
-func commandsModify(appPath, binaryName string) genny.RunFn {
+func commandsModify(appPath, binaryName string, version cosmosver.Version) genny.RunFn {
 	return func(r *genny.Runner) error {
 		cmdPath := filepath.Join(appPath, "cmd", binaryName, "cmd/commands.go")
 		f, err := r.Disk.Find(cmdPath)
 		if err != nil {
 			return err
+		}
+
+		if strings.Contains(f.String(), "rollserv.StartHandler[servertypes.Application]") {
+			return errors.New("rollkit is already installed")
+		}
+
+		if version.LT(cosmosver.StargateFiftyVersion) {
+			return errors.New("rollkit requires Ignite v28+ / Cosmos SDK v0.50+")
 		}
 
 		content, err := xast.AppendImports(
@@ -48,18 +58,33 @@ func commandsModify(appPath, binaryName string) genny.RunFn {
 			return err
 		}
 
+		// TODO(@julienrbrt) eventually use ast for simply replacing AddCommands or AddCommandsWithStartCmdOptions
 		const (
-			defaultServerOptions = "server.AddCommands(rootCmd, app.DefaultNodeHome, newApp, appExport, addModuleInitFlags)"
+			defaultv050ServerOptions       = "server.AddCommands(rootCmd, app.DefaultNodeHome, newApp, appExport, addModuleInitFlags)"
+			secondv050DefaultServerOptions = `server.AddCommandsWithStartCmdOptions(rootCmd, app.DefaultNodeHome, newApp, appExport, server.StartCmdOptions{
+				AddFlags: func(startCmd *cobra.Command) {
+					addModuleInitFlags(startCmd)
+				},
+			})`
+			thirdv050DefaultServerOptions = "server.AddCommandsWithStartCmdOptions(rootCmd, app.DefaultNodeHome, newApp, appExport, server.StartCmdOptions[servertypes.Application]{})"
+
 			rollkitServerOptions = `server.AddCommandsWithStartCmdOptions(
 				rootCmd,
 				app.DefaultNodeHome,
 				newApp, appExport,
 				server.StartCmdOptions{
-					AddFlags:            rollconf.AddFlags,
+					AddFlags: func(cmd *cobra.Command) {
+						rollconf.AddFlags(cmd)
+						addModuleInitFlags(cmd)
+					},
 					StartCommandHandler: rollserv.StartHandler[servertypes.Application],
 				})`
 		)
-		content = strings.ReplaceAll(content, defaultServerOptions, rollkitServerOptions)
+
+		// try all 3 possible default server options
+		content = strings.ReplaceAll(content, defaultv050ServerOptions, rollkitServerOptions)
+		content = strings.ReplaceAll(content, secondv050DefaultServerOptions, rollkitServerOptions)
+		content = strings.ReplaceAll(content, thirdv050DefaultServerOptions, rollkitServerOptions)
 
 		return r.File(genny.NewFileS(cmdPath, content))
 	}
