bd6fcc27d374e3909d661350d18a090635d8168d
Julien Robert <julien@rbrt.fr>
2024-06-07 11:15:34 +0200

    update test

--- marketplace/integration/app_test.go
+++ marketplace/integration/app_test.go
@@ -49,7 +49,7 @@ func TestMarketplace(t *testing.T) {
 		)),
 	))
 	gotList := listOutput.String()
-	require.True(strings.HasPrefix(gotList, "📦"), "unexpected output: %s", gotList)
+	require.True(strings.Contains(gotList, "marketplace :"), "unexpected output: %s", gotList)
 
 	infoOutput := &bytes.Buffer{}
 	env.Must(env.Exec("run marketplace info",
@@ -58,7 +58,7 @@ func TestMarketplace(t *testing.T) {
 				envtest.IgniteApp,
 				"marketplace",
 				"info",
-				"github.com/ignite/apps",
+				"explorer",
 			),
 			// all test outputs are going to the stdErr for no reason, but
 			// it's ok when we run the app. The output goes to stdout.
@@ -67,7 +67,7 @@ func TestMarketplace(t *testing.T) {
 		)),
 	))
 	gotInfo := infoOutput.String()
-	require.Contains(gotInfo, "Description:\tIgnite Apps")
+	require.Contains(gotInfo, "Description:\tEasy to use terminal chain explorer for testing your Ignite blockchains")
 }
 
 func assertGlobalPlugins(t *testing.T, expectedPlugins []pluginsconfig.Plugin) {
