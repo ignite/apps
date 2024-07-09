2310ba9f0560525ddc20afee86b9c2628bfc3379
Danilo Pantani <danpantani@gmail.com>
2024-07-10 01:11:54 +0200

    remove dashed name form tests

--- wasm/integration/app_test.go
+++ wasm/integration/app_test.go
@@ -17,7 +17,7 @@ func TestWasm(t *testing.T) {
 	var (
 		require     = require.New(t)
 		env         = envtest.New(t)
-		app         = env.Scaffold("github.com/apps/wasm-app")
+		app         = env.Scaffold("github.com/apps/wasmapp")
 		servers     = app.RandomizeServerPorts()
 		ctx, cancel = context.WithCancel(env.Ctx())
 	)
