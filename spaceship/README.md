33c23cdc94ac7488f853247dc6db99916a9f8a33
Danilo Pantani <danpantani@gmail.com>
2024-08-06 19:54:18 +0200

    feat: add real time logs for spaceship (#124)
    * Initialized with Ignite CLI
    
    * spaceship
    
    * use a pkg to deal with ssh connections
    
    * add shh deploy and dev logic
    
    * remove unused binary check
    
    * fix cmds
    
    * remove development mode
    
    * remove unused methods
    
    * initChain flag
    
    * add runner script
    
    * check os and arch and get logs
    
    * fix remote logs
    
    * remove exported var
    
    * fix script
    
    * fix the local tmp folder
    
    * add comments
    
    * add tarball extractor
    
    * add changelog and readme
    
    * skip tests
    
    * add arch map and improve readme
    
    * add upload progress bar
    
    * unknow hosts
    
    * unkown host
    
    * add real time logs
    
    * update go.mod
    
    * improve readme
    
    * improve logs
    
    ---------
    
    Co-authored-by: Developer Experience team at Ignite <hello@ignite.com>

--- spaceship/README.md
+++ spaceship/README.md
@@ -5,7 +5,7 @@ deploy blockchain applications via SSH.
 
 ## Prerequisites
 
-* Ignite CLI: Version v28.4.0 or higher is required.
+* Ignite CLI: Version `v28.4.0` or higher is required.
 * Blockchain Scaffold: A blockchain scaffolded using Ignite
 
 ## Usage
@@ -21,31 +21,37 @@ ignite spaceship deploy root@127.0.0.1 --key $HOME/.ssh/id_rsa --key-password ke
 
 Each command initiates a build of the blockchain binary and sets up the chain's home directory based on the
 configuration. The app then connects to the specified SSH server, establishes workspaces, transfers the binary, and
-executes it using a runner script. The workspaces are organized under $HOME/workspace/<chain-id> and include:
+executes it using a runner script. The workspaces are organized under `$HOME/workspace/<chain-id>` and include:
 
-- Binary Directory: $HOME/workspace/<chain-id>/bin - Contains the chain binary.
-- Home Directory: $HOME/workspace/<chain-id>/home - Stores chain data.
-- Log Directory: $HOME/workspace/<chain-id>/log - Holds logs of the running chain.
-- Runner Script: $HOME/workspace/<chain-id>/run.sh - A script to start the binary in the background using nohup.
-- PID File: $HOME/workspace/<chain-id>/spaceship.pid - Stores the PID of the currently running chain instance.
+- Binary Directory: `$HOME/workspace/<chain-id>/bin` - Contains the chain binary.
+- Home Directory: `$HOME/workspace/<chain-id>/home` - Stores chain data.
+- Log Directory: `$HOME/workspace/<chain-id>/log` - Holds logs of the running chain.
+- Runner Script: `$HOME/workspace/<chain-id>/run.sh` - A script to start the binary in the background using nohup.
+- PID File: `$HOME/workspace/<chain-id>/spaceship.pid` - Stores the PID of the currently running chain instance.
 
 ### Managing the Chain
 
 To manage your blockchain deployment, use the following commands:
 
-- Check Status:
+- Check status:
 
 ```sh
 ignite spaceship status root@127.0.0.1 --key $HOME/.ssh/id_rsa
 ```
 
-- View Logs:
+- View logs:
 
 ```sh
 ignite spaceship log root@127.0.0.1 --key $HOME/.ssh/id_rsa
 ```
 
-- Restart the Chain:
+- Watch the logs in real time:
+
+```sh
+ignite spaceship log root@127.0.0.1 --key $HOME/.ssh/id_rsa --real-time
+```
+
+- Restart the chain:
 
 ```sh
 ignite spaceship restart root@127.0.0.1 --key $HOME/.ssh/id_rsa
@@ -57,7 +63,7 @@ ignite spaceship restart root@127.0.0.1 --key $HOME/.ssh/id_rsa
 ignite spaceship stop root@127.0.0.1 --key $HOME/.ssh/id_rsa
 ```
 
-To redeploy the chain on the same server without overwriting the home directory, use the --init-chain flag to
+To redeploy the chain on the same server without overwriting the home directory, use the `--init-chain` flag to
 reinitialize the chain if necessary.
 
 ### Config
@@ -65,8 +71,8 @@ reinitialize the chain if necessary.
 You can override the default [chain configuration](https://docs.ignite.com/references/config#validators) by using the
 Ignite configuration file. Validators' node configuration files are stored in the data directory. By default, Spaceship
 initializes the chain locally in a temporary folder using the Ignite config file and then copies the configuration to
-the remote machine at $HOME/workspace/<chain-id>/home.
-Configuration resets are performed by Ignite when necessary, especially when using the --init-chain flag or if the chain
+the remote machine at `$HOME/workspace/<chain-id>/home`.
+Configuration resets are performed by Ignite when necessary, especially when using the `--init-chain` flag or if the chain
 was not previously initialized.
 
 Example Ignite config:
--- spaceship/cmd/ssh.go
+++ spaceship/cmd/ssh.go
@@ -7,10 +7,13 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/gookit/color"
 	ignitecmd "github.com/ignite/cli/v28/ignite/cmd"
+	"github.com/ignite/cli/v28/ignite/pkg/cliui"
 	"github.com/ignite/cli/v28/ignite/pkg/errors"
 	"github.com/ignite/cli/v28/ignite/services/plugin"
 	"github.com/schollz/progressbar/v3"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/ignite/apps/spaceship/pkg/ssh"
 	"github.com/ignite/apps/spaceship/pkg/tarball"
@@ -27,6 +30,10 @@ const (
 	flagRawKey      = "raw-key"
 	flagKeyPassword = "key-password"
 	flagInitChain   = "init-chain"
+	flagLines       = "lines"
+	flagRealTime    = "real-time"
+
+	statusConnecting = "Connecting..."
 )
 
 func executeSSH(cmd *plugin.ExecutedCommand, chain *plugin.ChainInfo) (*ssh.SSH, error) {
@@ -67,6 +74,9 @@ func executeSSH(cmd *plugin.ExecutedCommand, chain *plugin.ChainInfo) (*ssh.SSH,
 
 // ExecuteSSHStatus executes the ssh status subcommand.
 func ExecuteSSHStatus(ctx context.Context, cmd *plugin.ExecutedCommand, chain *plugin.ChainInfo) error {
+	session := cliui.New(cliui.StartSpinnerWithText(statusConnecting))
+	defer session.End()
+
 	c, err := executeSSH(cmd, chain)
 	if err != nil {
 		return err
@@ -81,12 +91,15 @@ func ExecuteSSHStatus(ctx context.Context, cmd *plugin.ExecutedCommand, chain *p
 	if err != nil {
 		return err
 	}
-	fmt.Println(status)
-	return nil
+
+	return session.Println(status)
 }
 
 // ExecuteSSHSStop executes the ssh stop subcommand.
 func ExecuteSSHSStop(ctx context.Context, cmd *plugin.ExecutedCommand, chain *plugin.ChainInfo) error {
+	session := cliui.New(cliui.StartSpinnerWithText(statusConnecting))
+	defer session.End()
+
 	c, err := executeSSH(cmd, chain)
 	if err != nil {
 		return err
@@ -101,12 +114,15 @@ func ExecuteSSHSStop(ctx context.Context, cmd *plugin.ExecutedCommand, chain *pl
 	if err != nil {
 		return err
 	}
-	fmt.Println(stop)
-	return nil
+
+	return session.Println(stop)
 }
 
 // ExecuteSSHRestart executes the ssh restart subcommand.
 func ExecuteSSHRestart(ctx context.Context, cmd *plugin.ExecutedCommand, chain *plugin.ChainInfo) error {
+	session := cliui.New(cliui.StartSpinnerWithText(statusConnecting))
+	defer session.End()
+
 	c, err := executeSSH(cmd, chain)
 	if err != nil {
 		return err
@@ -121,12 +137,21 @@ func ExecuteSSHRestart(ctx context.Context, cmd *plugin.ExecutedCommand, chain *
 	if err != nil {
 		return err
 	}
-	fmt.Println(restart)
-	return nil
+
+	return session.Println(restart)
 }
 
 // ExecuteSSHLog executes the ssh log subcommand.
 func ExecuteSSHLog(ctx context.Context, cmd *plugin.ExecutedCommand, chain *plugin.ChainInfo) error {
+	session := cliui.New(cliui.StartSpinnerWithText(statusConnecting))
+	defer session.End()
+
+	var (
+		flags       = plugin.Flags(cmd.Flags)
+		lines, _    = flags.GetInt(flagLines)
+		realTime, _ = flags.GetBool(flagRealTime)
+	)
+
 	c, err := executeSSH(cmd, chain)
 	if err != nil {
 		return err
@@ -137,16 +162,48 @@ func ExecuteSSHLog(ctx context.Context, cmd *plugin.ExecutedCommand, chain *plug
 		return ErrServerNotInitialized
 	}
 
-	log, err := c.LatestLog()
+	logs, err := c.LatestLog(lines)
 	if err != nil {
 		return err
 	}
-	fmt.Println(string(log))
+	_ = session.Println(logs)
+
+	if realTime {
+		// Create a buffered channel to receive log lines.
+		logChannel := make(chan string, 100)
+		g, ctx := errgroup.WithContext(ctx)
+
+		// Start the FollowLog method in a goroutine using errgroup
+		g.Go(func() error {
+			return c.FollowLog(ctx, logChannel)
+		})
+
+		// Start a goroutine to consume log lines
+		g.Go(func() error {
+			for {
+				select {
+				case line := <-logChannel:
+					_ = session.Print(line)
+				case <-ctx.Done():
+					return ctx.Err()
+				}
+			}
+		})
+
+		// Wait for all goroutines to complete
+		if err := g.Wait(); err != nil && !errors.Is(err, context.Canceled) {
+			return err
+		}
+	}
+
 	return nil
 }
 
 // ExecuteSSHDeploy executes the ssh deploy subcommand.
 func ExecuteSSHDeploy(ctx context.Context, cmd *plugin.ExecutedCommand, chain *plugin.ChainInfo) error {
+	session := cliui.New(cliui.StartSpinnerWithText(statusConnecting))
+	defer session.End()
+
 	flags := plugin.Flags(cmd.Flags)
 
 	localDir, err := os.MkdirTemp(os.TempDir(), "spaceship")
@@ -169,6 +226,8 @@ func ExecuteSSHDeploy(ctx context.Context, cmd *plugin.ExecutedCommand, chain *p
 		return err
 	}
 	defer c.Close()
+
+	_ = session.Println(color.Yellow.Sprintf("Building chain binary using Ignite:"))
 
 	target, err := c.Target(ctx)
 	if err != nil {
@@ -227,26 +286,29 @@ func ExecuteSSHDeploy(ctx context.Context, cmd *plugin.ExecutedCommand, chain *p
 		return errors.Errorf("zero files extracted from the tarball %s", localChainTarball)
 	}
 
-	bar.Describe("uploading chain binary")
+	bar.Describe("Uploading chain binary")
 	binPath, err := c.UploadBinary(extracted[0], progressCallback)
 	if err != nil {
 		return err
 	}
+	_ = session.Println(color.Yellow.Sprintf("Chain binary uploaded to '%s'\n", binPath))
 
 	home := c.Home()
 	if initChain || !c.HasGenesis(ctx) {
-		// Init the chain.
+		_ = session.Println(color.Yellow.Sprintf("Initializing the chain home folder using Ignite:"))
+
 		igniteChainInitCmd := ignitecmd.NewChainInit()
 		igniteChainInitCmd.SetArgs([]string{"-p", chain.AppPath, "--home", localChainHome}) // TODO add verbose flag after merge and backport this one https://github.com/ignite/cli/pull/4286
 		if err := igniteChainInitCmd.ExecuteContext(ctx); err != nil {
 			return err
 		}
 
-		bar.Describe("uploading chain home folder")
-		home, err = c.UploadHome(ctx, localChainHome, progressCallback)
+		bar.Describe("Uploading chain home folder")
+		homeFiles, err := c.UploadHome(ctx, localChainHome, progressCallback)
 		if err != nil {
 			return err
 		}
+		_ = session.Println(color.Yellow.Sprintf("Uploaded files: \n- %s\n", strings.Join(homeFiles, "\n- ")))
 	}
 
 	// Create the runner script.
@@ -255,7 +317,7 @@ func ExecuteSSHDeploy(ctx context.Context, cmd *plugin.ExecutedCommand, chain *p
 		return err
 	}
 
-	bar.Describe("uploading runner script")
+	bar.Describe("Uploading runner script")
 	if _, err := c.UploadRunnerScript(localRunScriptPath, progressCallback); err != nil {
 		return err
 	}
@@ -264,7 +326,7 @@ func ExecuteSSHDeploy(ctx context.Context, cmd *plugin.ExecutedCommand, chain *p
 	if err != nil {
 		return err
 	}
-	fmt.Println(start)
+	_ = session.Println("")
 
-	return nil
+	return session.Println(color.Blue.Sprintf(start))
 }
