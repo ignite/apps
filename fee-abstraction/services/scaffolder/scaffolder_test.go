6dc2dd1c09b0afc51f154b54173409d168833c07
Danilo Pantani <danpantani@gmail.com>
2024-09-17 15:28:04 +0200

    add more test cases and fix old cases

--- fee-abstraction/services/scaffolder/scaffolder_test.go
+++ fee-abstraction/services/scaffolder/scaffolder_test.go
@@ -3,19 +3,26 @@ package scaffolder
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/ignite/cli/v28/ignite/pkg/cosmosver"
 	"github.com/ignite/cli/v28/ignite/pkg/errors"
-	"github.com/stretchr/testify/require"
 )
 
 func Test_assertSupportedCosmosSDKVersion(t *testing.T) {
-	v100, err := cosmosver.Parse("v1.0.0")
+	v0391, err := cosmosver.Parse("v0.39.1")
 	require.NoError(t, err)
-	v0501, err := cosmosver.Parse("v0.50.1")
+	v0449, err := cosmosver.Parse("v0.44.9")
 	require.NoError(t, err)
 	v0450, err := cosmosver.Parse("v0.45.0")
 	require.NoError(t, err)
-	v0391, err := cosmosver.Parse("v0.39.1")
+	v0500, err := cosmosver.Parse("v0.50.0")
+	require.NoError(t, err)
+	v0501, err := cosmosver.Parse("v0.50.1")
+	require.NoError(t, err)
+	v0510, err := cosmosver.Parse("0.51.0")
+	require.NoError(t, err)
+	v100, err := cosmosver.Parse("v1.0.0")
 	require.NoError(t, err)
 
 	tests := []struct {
@@ -28,10 +35,6 @@ func Test_assertSupportedCosmosSDKVersion(t *testing.T) {
 			v:    v0501,
 		},
 		{
-			name: "Supported Cosmos SDK version (greater than)",
-			v:    v100,
-		},
-		{
 			name: "Unsupported Cosmos SDK version",
 			v:    v0450,
 			err:  errors.Errorf(errOldCosmosSDKVersionStr, v0450),
@@ -40,6 +43,45 @@ func Test_assertSupportedCosmosSDKVersion(t *testing.T) {
 			name: "Unsupported Cosmos SDK version",
 			v:    v0391,
 			err:  errors.Errorf(errOldCosmosSDKVersionStr, v0391),
+		},
+
+		{
+			name: "Supported Cosmos SDK version (equal to v0.50.1)",
+			v:    v0501,
+		},
+		{
+			name: "Supported Cosmos SDK version (greater than v1.0.0)",
+			v:    v100,
+			err:  errors.Errorf(errNewCosmosSDKVersionStr, v100, v0510),
+		},
+		{
+			name: "Unsupported Cosmos SDK version (less than v0.45.0)",
+			v:    v0391,
+			err:  errors.Errorf(errOldCosmosSDKVersionStr, v0391),
+		},
+		{
+			name: "Unsupported Cosmos SDK version (less than v0.50.1)",
+			v:    v0450,
+			err:  errors.Errorf(errOldCosmosSDKVersionStr, v0450),
+		},
+		{
+			name: "Edge case: exact boundary of supported version v0.50.1",
+			v:    v0501,
+		},
+		{
+			name: "Edge case: exact boundary of unsupported version v0.39.1",
+			v:    v0391,
+			err:  errors.Errorf(errOldCosmosSDKVersionStr, v0391),
+		},
+		{
+			name: "Lower boundary case: supported version v0.50.0 (if supported)",
+			v:    v0500,
+			err:  errors.Errorf(errOldCosmosSDKVersionStr, v0500),
+		},
+		{
+			name: "Lower boundary case: unsupported version v0.44.9 (if unsupported)",
+			v:    v0449,
+			err:  errors.Errorf(errOldCosmosSDKVersionStr, v0449),
 		},
 	}
 	for _, tt := range tests {
