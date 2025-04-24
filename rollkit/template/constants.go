9d51c52305be37356a1ecadab8733b77842e1c37
Julien Robert <julien@rbrt.fr>
2025-04-24 17:05:34 +0200

    bump commit

--- rollkit/template/constants.go
+++ rollkit/template/constants.go
@@ -9,7 +9,7 @@ const (
 
 const (
 	GoExecPackage = "github.com/rollkit/go-execution-abci"
-	GoExecVersion = "v0.2.1-0.20250423084551-59072faf8f18" // TODO(@julienrbrt): use tag when available
+	GoExecVersion = "v0.2.1-0.20250424150026-b82d5e904bb8" // TODO(@julienrbrt): use tag when available
 
 	RollkitPackage = "github.com/rollkit/rollkit"
 	RollkitVersion = "v0.14.2-0.20250423102443-62dd136ae02e" // TODO(@julienrbrt): use tag when available
