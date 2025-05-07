6df1530c06cd6c19f74f85c3b6c428ca449d6f4d
Julien Robert <julien@rbrt.fr>
2025-05-07 15:20:36 +0200

    typo

--- rollkit/template/constants.go
+++ rollkit/template/constants.go
@@ -9,7 +9,7 @@ const (
 
 const (
 	GoExecPackage = "github.com/rollkit/go-execution-abci"
-	GoExecVersion = "v0.14.2-0.20250506155912-dccebc0cab3e" // TODO(@julienrbrt): use tag when available
+	GoExecVersion = "v0.2.1-0.20250507114132-4abb42ac7c60" // TODO(@julienrbrt): use tag when available
 
 	RollkitPackage = "github.com/rollkit/rollkit"
 	RollkitVersion = "v0.14.2-0.20250506085607-c574d7c79224" // TODO(@julienrbrt): use tag when available
