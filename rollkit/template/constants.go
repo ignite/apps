2d031978bf17478fec73afbe15d505fac021bb02
Julien Robert <julien@rbrt.fr>
2025-06-24 23:59:45 +0200

    feat(rollkit): add migration command

--- rollkit/template/constants.go
+++ rollkit/template/constants.go
@@ -5,12 +5,13 @@ const (
 
 	RollkitV0XStartHandler = "rollserv.StartHandler"
 	RollkitV1XStartHandler = "abciserver.StartHandler"
+	rollkitV1MigrateCmd    = "abciserver.MigrateToRollkitCmd()"
 )
 
 const (
 	GoExecPackage = "github.com/rollkit/go-execution-abci"
-	GoExecVersion = "v0.2.1-0.20250521112925-012f1f753eb4" // TODO(@julienrbrt): use tag when available
+	GoExecVersion = "v0.2.1-0.20250624195837-8bbd403344b4" // TODO(@julienrbrt): use tag when available
 
 	RollkitPackage = "github.com/rollkit/rollkit"
-	RollkitVersion = "v0.14.2-0.20250521111736-d1e1eee29233" // TODO(@julienrbrt): use tag when available
+	RollkitVersion = "v0.14.2-0.20250624162917-8bb1ba71af39" // TODO(@julienrbrt): use tag when available
 )
