d4b11b2e6316ea014c6d6eb10efe8994d81d1aa5
Julien Robert <julien@rbrt.fr>
2025-08-19 11:00:27 +0200

    chore(evolve): use tagged version

--- evolve/template/constants.go
+++ evolve/template/constants.go
@@ -10,11 +10,10 @@ const (
 
 const (
 	GoExecPackage = "github.com/evstack/ev-abci"
-	GoExecVersion = "v0.3.1-0.20250818181501-f014411689fd" // main until tag
+	GoExecVersion = "v0.3.1-0.20250818181501-f014411689fd"
 
 	EvNodePackage = "github.com/evstack/ev-node"
-	EvNodeVersion = "v1.0.0-beta.2.0.20250818133040-d096a24e7052" // main until tag
+	EvNodeVersion = "v1.0.0-beta.2.0.20250818133040-d096a24e7052"
 
-	EvNodeDaCmd     = "github.com/evstack/ev-node/da/cmd/local-da"
-	EvNodeDaVersion = "v1.0.0-beta.1"
+	EvNodeDaCmd = "github.com/evstack/ev-node/da/cmd/local-da"
 )
