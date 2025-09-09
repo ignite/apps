5a9eaaf6501588f9805208038cbe9dbaaee6d86d
Julien Robert <julien@rbrt.fr>
2025-09-09 16:13:52 +0200

    feat(evolve): init namespace with good values

--- evolve/template/constants.go
+++ evolve/template/constants.go
@@ -9,8 +9,8 @@ const (
 )
 
 const (
-	GoExecPackage = "github.com/evstack/ev-abci"
-	GoExecVersion = "v0.3.1-0.20250818181501-f014411689fd" // main until tag
+	EvABCIPackage = "github.com/evstack/ev-abci"
+	EvABCIVersion = "v0.3.1-0.20250818181501-f014411689fd" // main until tag
 
 	EvNodePackage = "github.com/evstack/ev-node"
 	EvNodeVersion = "v1.0.0-beta.2.0.20250818133040-d096a24e7052" // main until tag
