1263165d9079a2973c98238a05284ef5e92d6064
Julien Robert <julien@rbrt.fr>
2025-09-24 20:58:50 +0200

    updates

--- evolve/template/constants.go
+++ evolve/template/constants.go
@@ -15,5 +15,9 @@ const (
 	EvNodePackage = "github.com/evstack/ev-node"
 	EvNodeVersion = "v1.0.0-beta.5"
 
+	GoHeaderPackageFork = "github.com/julienrbrt/go-header"
+	GoHeaderVersionFork = "v0.0.0-20250909151551-cb11b091bf58"
+	GoHeaderPackage     = "github.com/celestiaorg/go-header"
+
 	EvNodeDaCmd = "github.com/evstack/ev-node/da/cmd/local-da"
 )
