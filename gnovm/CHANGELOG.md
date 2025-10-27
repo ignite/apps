3011889ea2d8689f7f84ae635fbdb9af34bdef8a
julienrbrt <julien@rbrt.fr>
2025-10-27 17:06:46 +0100

    chore(gnovm): use aib gno fork (#243)
    * chore(gnovm): use aib gno fork
    
    * chore: add cl

--- gnovm/CHANGELOG.md
+++ gnovm/CHANGELOG.md
@@ -1,5 +1,9 @@
 # GnoVM App Changelog
 
+## [`v0.1.1`](https://github.com/ignite/apps/releases/tag/gnovm/v0.1.1)
+
+- [#243](https://github.com/ignite/apps/pull/243) Use AiB Gno fork.
+
 ## [`v0.1.0`](https://github.com/ignite/apps/releases/tag/gnovm/v0.1.0)
 
 - First release of the GnoVM app compatible with Ignite >= v29.5.0
--- gnovm/template/constants.go
+++ gnovm/template/constants.go
@@ -2,8 +2,8 @@ package template
 
 const (
 	GnolangPackage     = "github.com/gnolang/gno"
-	GnolangForkPackage = "github.com/julienrbrt/gno"
-	GnolangForkVersion = "v0.0.0-20251016194523-cc39946f0d00"
+	GnolangForkPackage = "github.com/allinbits/gno"
+	GnolangForkVersion = "v0.0.0-20251027152958-cf2243d301a3"
 
 	GnoVMModulePackage = "github.com/ignite/gnovm"
 	GnoVMModuleVersion = "v0.1.0-alpha.1"
