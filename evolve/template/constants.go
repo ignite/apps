e6a6682a6f275e658d767d57d49c0d3a9720ba20
Julien Robert <julien@rbrt.fr>
2025-12-17 15:18:09 +0100

    bump to main ev-abci

--- evolve/template/constants.go
+++ evolve/template/constants.go
@@ -9,10 +9,10 @@ const (
 
 const (
 	EvABCIPackage = "github.com/evstack/ev-abci"
-	EvABCIVersion = "v1.0.0-beta.3"
+	EvABCIVersion = "v1.0.0-beta.3.0.20251217135843-7ae596a89474" // TODO(@julienrbrt): latest main: bump after tag ev-abci
 
 	EvNodePackage = "github.com/evstack/ev-node"
-	EvNodeVersion = "v1.0.0-beta.10.0.20251216132820-afcd6bd9b354" // bump after https://github.com/evstack/ev-node/issues/2906
+	EvNodeVersion = "v1.0.0-beta.10.0.20251216132820-afcd6bd9b354" // TODO(@julienrbrt): latest main: bump after tag ev-node
 
 	GoHeaderPackageFork = "github.com/julienrbrt/go-header"
 	GoHeaderVersionFork = "v0.0.0-20250909151551-cb11b091bf58"
