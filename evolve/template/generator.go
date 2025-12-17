f3b27b7cfe6ce467c9d2e7b51a9491d048a1ef2b
Julien Robert <julien@rbrt.fr>
2025-12-17 15:39:13 +0100

    chore(evolve): prepare for release

--- evolve/template/generator.go
+++ evolve/template/generator.go
@@ -73,10 +73,6 @@ func updateDependencies(appPath string) error {
 	// add required replaces
 	gomod.AddReplace(GoHeaderPackage, "", GoHeaderPackageFork, GoHeaderVersionFork)
 
-	// add temporary replaces
-	// TODO(@julienrbrt): remove after tagged version of ev-abci and ev-node
-	gomod.AddReplace("github.com/evstack/ev-node/core", "", "github.com/evstack/ev-node/core", "v1.0.0-beta.5.0.20251216132820-afcd6bd9b354")
-
 	// save go.mod
 	data, err := gomod.Format()
 	if err != nil {
