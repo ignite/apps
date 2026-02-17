bad6ab4e36a5f184e312bce06e564d8f2a3dbff4
Julien Robert <julien@rbrt.fr>
2026-02-17 18:09:09 +0100

    bump to version

--- evolve/template/constants.go
+++ evolve/template/constants.go
@@ -9,10 +9,10 @@ const (
 
 const (
 	EvABCIPackage = "github.com/evstack/ev-abci"
-	EvABCIVersion = "v1.0.0-beta.4"
+	EvABCIVersion = "v1.0.0-beta.5"
 
 	EvNodePackage = "github.com/evstack/ev-node"
-	EvNodeVersion = "v1.0.0-rc.3"
+	EvNodeVersion = "v1.0.0-rc.4.0.20260216131057-1da76345e4b9"
 
 	EvNodeDaCmd = "github.com/evstack/ev-node/tools/local-da"
 )
