dcdb2ddfc18e66a10fef709fc9aef19e6da0379b
Julien Robert <julien@rbrt.fr>
2026-02-26 10:58:58 +0100

    chore: bump gnovm

--- gnovm/template/constants.go
+++ gnovm/template/constants.go
@@ -3,8 +3,8 @@ package template
 const (
 	GnolangPackage     = "github.com/gnolang/gno"
 	GnolangForkPackage = "github.com/allinbits/gno"
-	GnolangForkVersion = "v0.0.0-20251027152958-cf2243d301a3"
+	GnolangForkVersion = "v0.0.0-20260225225412-eaf5d1ebb836"
 
 	GnoVMModulePackage = "github.com/ignite/gnovm"
-	GnoVMModuleVersion = "v0.1.0-alpha.1"
+	GnoVMModuleVersion = "v0.1.0-beta.1"
 )
